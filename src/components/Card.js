a09a1e2dd8ddd1a3cce9fafa399a5ef2d7be30a8
mnaauval <mnaauval20@gmail.com>
2021-10-02 00:56:27 +0700

    Card.js fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,25 @@
-// import React, { useState } from "react";
-import React from "react";
+import React, { useState } from "react";
+// import React from "react";
 import CardItem from "./CardItem";
-// import items from "../helpers/CardList";
+import items from "../helpers/CardList";
 import "../styles/Card.css";
-import image2 from "../images/img-2.jpg";
-import image3 from "../images/img-3.jpg";
-import image4 from "../images/img-4.jpg";
-import image8 from "../images/img-8.jpg";
-import image9 from "../images/img-9.jpg";
+// import image2 from "../images/img-2.jpg";
+// import image3 from "../images/img-3.jpg";
+// import image4 from "../images/img-4.jpg";
+// import image8 from "../images/img-8.jpg";
+// import image9 from "../images/img-9.jpg";
 
 function Card() {
-  // const [cardList] = useState(items);
+  const [cardList] = useState(items);
   return (
     <div className="cards">
       <h1>Check out these EPIC Destinations</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
-          {/* <ul className="cards__item">
+          <ul className="cards__item">
             <CardItem cardList={cardList} />
-          </ul> */}
-          <ul className="cards__items">
+          </ul>
+          {/* <ul className="cards__items">
             <CardItem src={image9} text="Explore the hidden waterfall deep inside the Amazon Jungle" label="Adventure" path="/services" />
             <CardItem src={image2} text="Travel through the Islands of Bali in a Private Cruise" label="Luxury" path="/services" />
           </ul>
@@ -27,7 +27,7 @@ function Card() {
             <CardItem src={image3} text="Set Sail in the Atlantic Ocean visiting Uncharted Waters" label="Mystery" path="/services" />
             <CardItem src={image4} text="Experience Football on Top of the Himilayan Mountains" label="Adventure" path="/products" />
             <CardItem src={image8} text="Ride through the Sahara Desert on a guided camel tour" label="Adrenaline" path="/sign-up" />
-          </ul>
+          </ul> */}
         </div>
       </div>
     </div>
--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,36 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-function CardItem(props) {
+// function CardItem(props) {
+function CardItem({ cardList }) {
   return (
     <>
-      <li key={props.id} className="cards__item">
-        <Link className="cards__item__link" to={props.path}>
-          <figure className="cards__item__pic-wrap" data-category={props.label}>
-            <img className="cards__item__img" alt="Travel" src={props.src} />
-          </figure>
-          <div className="cards__item__info">
-            <h5 className="cards__item__text">{props.text}</h5>
-          </div>
-        </Link>
-      </li>
+      {cardList.slice(0, 3).map((list) => {
+        return (
+          <CardItemStyled>
+            <Link className="cards__item__link" to={list.path}>
+              <figure className="cards__item__pic-wrap" data-category={list.label}>
+                <img className="cards__item__img" alt="Travel" src={list.src} />
+              </figure>
+              <div className="cards__item__info">
+                <h5 className="cards__item__text">{list.text}</h5>
+              </div>
+            </Link>
+          </CardItemStyled>
+          // <li key={list.id} className="cards__item">
+
+          // </li>
+        );
+      })}
     </>
   );
 }
 
+const CardItemStyled = styled.li`
+  display: flex;
+  flex: 1;
+  margin: 0 1rem;
+  border-radius: 10px;
+`;
+
 export default CardItem;
 
-// {cardList.slice(0, 3).map((list) => {
-//   return (
-//     <li key={list.id} className="cards__item">
-//       <Link className="cards__item__link" to={list.path}>
-//         <figure className="cards__item__pic-wrap" data-category={list.label}>
-//           <img className="cards__item__img" alt="Travel" src={list.src} />
-//         </figure>
-//         <div className="cards__item__info">
-//           <h5 className="cards__item__text">{list.text}</h5>
-//         </div>
-//       </Link>
-//     </li>
-//   );
-// })}
+{
+  /* <li key={props.id} className="cards__item">
+  <Link className="cards__item__link" to={props.path}>
+    <figure className="cards__item__pic-wrap" data-category={props.label}>
+      <img className="cards__item__img" alt="Travel" src={props.src} />
+    </figure>
+    <div className="cards__item__info">
+      <h5 className="cards__item__text">{props.text}</h5>
+    </div>
+  </Link>
+</li>; */
+}
